f6c50badb82431942461c1360849df658e759c5b
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-05-19 00:42:54 +0530

    Fixed a bug

--- PPIDSG/update.py
+++ PPIDSG/update.py
@@ -95,7 +95,7 @@ class LocalUpdate(object):
                 # input image data
                 # Now none of the images are encrypted
                 img = real_A.numpy().astype("float32")
-                real_B = torch.from_numpy(np.transpose(img, (0, 3, 1, 2)) / 255.0)
+                real_B = torch.from_numpy(img)
                 real_B = Variable(real_B.to(device))
                 real_A = Variable(real_A.to(device))
                 label = label.to(device)
